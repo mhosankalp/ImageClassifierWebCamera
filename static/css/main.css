75df38a258e66d4a463e41196b3b3980656b7b8b
mhosankalp <mho.sankalp@gmail.com>
2019-09-01 00:45:57 -0400

    Add files via upload

---
+++ static/css/main.css
@@ -0,0 +1,34 @@
+.booth {
+	width:400px;
+	background-color:#ccc;
+	border:10px solid #ddd;
+	margin:0 auto;
+}
+
+.booth-capture-button {
+	display:block;
+	margin:10px 0;
+	padding:10px 20px;
+	background-color:cornflowerblue;
+	color:#fff;
+	text-align: center;
+	text-decoration: none;
+}
+
+#canvas {
+	display:none;
+}
+
+.loader {
+    border: 8px solid #f3f3f3; /* Light grey */
+    border-top: 8px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
